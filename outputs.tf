ee38df465fadc37eec08d8df0a686af7ffa29fc4
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "mssql_server_dns_aliases" {
   description = "All mssql_server_dns_alias resources"
   value       = azurerm_mssql_server_dns_alias.mssql_server_dns_aliases
 }
+output "mssql_server_dns_aliases_dns_record" {
+  description = "List of dns_record values across all mssql_server_dns_aliases"
+  value       = [for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : v.dns_record]
+}
+output "mssql_server_dns_aliases_mssql_server_id" {
+  description = "List of mssql_server_id values across all mssql_server_dns_aliases"
+  value       = [for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : v.mssql_server_id]
+}
+output "mssql_server_dns_aliases_name" {
+  description = "List of name values across all mssql_server_dns_aliases"
+  value       = [for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : v.name]
+}
 
