020b49a89860fbd035d5e76292b47f7d2f431ccc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "mssql_server_dns_aliases_id" {
+  description = "Map of id values across all mssql_server_dns_aliases, keyed the same as var.mssql_server_dns_aliases"
+  value       = { for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : k => v.id }
+}
 output "mssql_server_dns_aliases_dns_record" {
   description = "Map of dns_record values across all mssql_server_dns_aliases, keyed the same as var.mssql_server_dns_aliases"
   value       = { for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : k => v.dns_record }
