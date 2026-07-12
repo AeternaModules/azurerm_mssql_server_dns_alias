b04ab44155f14a97ec22d8d5419d3f27ba81c20e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "mssql_server_dns_aliases" {
-  description = "All mssql_server_dns_alias resources"
-  value       = azurerm_mssql_server_dns_alias.mssql_server_dns_aliases
-}
 output "mssql_server_dns_aliases_dns_record" {
-  description = "List of dns_record values across all mssql_server_dns_aliases"
-  value       = [for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : v.dns_record]
+  description = "Map of dns_record values across all mssql_server_dns_aliases, keyed the same as var.mssql_server_dns_aliases"
+  value       = { for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : k => v.dns_record }
 }
 output "mssql_server_dns_aliases_mssql_server_id" {
-  description = "List of mssql_server_id values across all mssql_server_dns_aliases"
-  value       = [for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : v.mssql_server_id]
+  description = "Map of mssql_server_id values across all mssql_server_dns_aliases, keyed the same as var.mssql_server_dns_aliases"
+  value       = { for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : k => v.mssql_server_id }
 }
 output "mssql_server_dns_aliases_name" {
-  description = "List of name values across all mssql_server_dns_aliases"
-  value       = [for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : v.name]
+  description = "Map of name values across all mssql_server_dns_aliases, keyed the same as var.mssql_server_dns_aliases"
+  value       = { for k, v in azurerm_mssql_server_dns_alias.mssql_server_dns_aliases : k => v.name }
 }
 
